bb757ce4e726d2e801db7136c1e52d75631858d3
Khrylx <khrylx@gmail.com>
2017-10-05 14:09:10 -0400

    small changes.

--- gym/envs/mujoco/mujoco_env.py
+++ gym/envs/mujoco/mujoco_env.py
@@ -39,7 +39,7 @@ class MujocoEnv(gym.Env):
         self.init_qpos = self.data.qpos.ravel().copy()
         self.init_qvel = self.data.qvel.ravel().copy()
         observation, _reward, done, _info = self._step(np.zeros(self.model.nu))
-        assert not done
+        # assert not done
         self.obs_dim = observation.size
 
         bounds = self.model.actuator_ctrlrange.copy()
